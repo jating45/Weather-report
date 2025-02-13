35dd4088664a5bc6a91eb3f5f8eb00be6956c73d
Jatin Gupta <128054450+jating45@users.noreply.github.com>
2025-02-13 17:38:26 +0530

    Add files via upload

---
+++ src/Components/Card.js
@@ -0,0 +1,10 @@
+const Card = ({ className, children, ...props }) => {
+    return (
+      <div className={`card ${className}`} {...props}>
+        {children}
+      </div>
+    );
+  };
+  
+  export default Card;
+  
---
+++ src/Components/CardContent.js
@@ -0,0 +1,6 @@
+const CardContent = ({ children }) => {
+    return <div className="card-body">{children}</div>;
+  };
+  
+  export default CardContent;
+  
